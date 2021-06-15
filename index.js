cea5cd9dca63dc3045abd4ddc48319db23e0cfbe
William Badger <wkbadger@gmail.com>
2021-06-14 21:33:38 -0500

    Completed Assignment Again

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ main.remove();
 
 const newHeader = document.createElement('h1');
 document.body.appendChild(newHeader);
+
 newHeader.id = "victory";
+
 newHeader.innerHTML = "<hi>Will is the champion</h>";
